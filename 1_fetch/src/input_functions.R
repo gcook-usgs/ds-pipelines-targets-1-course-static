25ddde0f176299c14b52d359ab838ebdc4be254e
Gericke Cook <gcook@usgs.gov>
2023-03-08 17:24:28 -0700

    Targets makefile exercise submitted for review

--- 1_fetch/src/input_functions.R
+++ 1_fetch/src/input_functions.R
@@ -1,22 +1,11 @@
 
-#' @description install and load required packages
-#' @param pkg - a list of R packages required to run the code
-
-
-check.packages <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
 #' @description download ScienceBase file by item number
-#' @param SB_item_file - the USGS ScienceBase item number that corresponds to a file
+#' @param SB_item_ID - the USGS ScienceBase item number that corresponds to a file
+#' @param SBFileNames - the csv file corresponding to the item ID
 #' @return a file pulled from ScienceBase and saved locally as CSV
 #' 
-get.SBfile <- function(outpath,SBFileNames, SB_item_ID){
-  mendota_file <- file.path(outpath, 'model_RMSEs.csv')
-  return(item_file_download(SB_item_ID, names = SBFileNames, destinations = mendota_file, overwrite_file = TRUE))
+get.SBfile <- function(SB_item_ID, SBFileNames, outFile){
+  return(item_file_download(SB_item_ID, names = SBFileNames, destinations = outFile, overwrite_file = TRUE))
 }
 
 
